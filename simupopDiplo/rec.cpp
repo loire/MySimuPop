f22e444ccb83457df4cf9b8ed69827e5b8bc6ead
loire <etienneloire@gmail.com>
2013-01-21 16:50:28 +0100

    Like the last message, it's just a test

--- simupopDiplo/rec.cpp
+++ simupopDiplo/rec.cpp
@@ -29,6 +29,8 @@ boost::dynamic_bitset<> RandomMask(int N)
 
 void rec(chr_diplo &res, chr_diplo &c1, chr_diplo &c2, double Sz, int nS)
 {
+
+	cout << "change in rec function"
 	vector<int> pos;
 	int j;
 	int nS_1 = nS - 1;
